e77005079ebdfe9dc3263da9e2ff9cd4c280cde4
Nodirbek Isaev <nodirbekisaev5@gmail.com>
2026-09-23 16:36:26 +0900

    feat:  ENV , Database and Backend Framework for new project is completed

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import path from "node:path/win32";
+
+/** 1- ENRTANCE **/
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+/** 1- SESSIONS **/
+
+/** 1- VIEWS **/
+app.set("views", path.join(__dirname, " views"));
+app.set("view engine", "ejs");
+
+/** 1- ROUTERS **/
+
+export default app; //module.exports
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
-console.log("Hello World");
+import dotenv from "dotenv";
+dotenv.config();
 
-import moment from "moment";
-
-const currentTime = moment().format("YYYY MM DD");
-console.log(currentTime);
-
-// Architectural pattern: MVC, Dependency InjectionI, MVP
-
-// Design pattern: Middleware, Decorator
+import mongoose from "mongoose";
+import app from "./app";
+mongoose
+  .connect(process.env.MONGO_URL as string, {})
+  .then((data) => {
+    console.log("MongoDB connection succeed");
+    const PORT = process.env.PORT ?? 3003;
+    app.listen(PORT, function () {
+      console.log(`The server is running successfully on prot: ${PORT}`);
+    });
+  })
+  .catch((err) => console.log("ERROR on connection on MongoDB", err));
