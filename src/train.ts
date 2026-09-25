48f94d285afb11debf4be0dc81a14c5d6392f23b
Nodirbek Isaev <nodirbekisaev5@gmail.com>
2026-09-26 02:10:42 +0900

    feat: MIT TASK -  N completed

--- src/train.ts
+++ src/train.ts
@@ -1,17 +1,32 @@
+// TASK N:
+
+// Shunday function yozing, u string qabul qilsin va string palindrom yani togri oqilganda ham, orqasidan oqilganda ham bir hil oqiladigan soz ekanligini aniqlab boolean qiymat qaytarsin.
+
+// MASALAN: palindromCheck("dad") return true;  palindromCheck("son") return false;
+function palindromCheck(str: string) {
+  return str === str.split("").reverse().join("");
+}
+
+const result = palindromCheck("dad");
+const result2 = palindromCheck("son");
+
+console.log("Result1: ", result);
+console.log("Result2: ", result2);
+
 // TASK M:
 
 // Shunday function yozing, u raqamlardan tashkil topgan array qabul qilsin va array ichidagi har bir raqam uchun raqamni ozi va hamda osha raqamni kvadratidan tashkil topgan object hosil qilib, hosil bolgan objectlarni array ichida qaytarsin.
 // MASALAN: getSquareNumbers([1, 2, 3]) return [{number: 1, square: 1}, {number: 2, square: 4}, {number: 3, square: 9}];
-function getSquareNumbers(numbs: number[]) {
-  let result = [];
-  for (let i = 0; i < numbs.length; i++) {
-    let number = numbs[i];
-    result.push({ number: number, square: number * number });
-  }
-  return result;
-}
-const answer = getSquareNumbers([1, 2, 3]);
-console.log(answer);
+// function getSquareNumbers(numbs: number[]) {
+//   let result = [];
+//   for (let i = 0; i < numbs.length; i++) {
+//     let number = numbs[i];
+//     result.push({ number: number, square: number * number });
+//   }
+//   return result;
+// }
+// const answer = getSquareNumbers([1, 2, 3]);
+// console.log(answer);
 
 // Shunday function yozing, u string qabul qilsin va string ichidagi hamma sozlarni chappasiga yozib va sozlar ketma-ketligini buzmasdan stringni qaytarsin.
 // MASALAN: reverseSentence("we like coding!") return "ew ekil gnidoc";
--- train.js
+++ train.js
@@ -1,10 +1,25 @@
-function getSquareNumbers(numbs) {
-  let result = [];
-  for (let i = 0; i < numbs.length; i++) {
-    let number = numbs[i];
-    result.push({ number: number, square: number * number });
-  }
-  return result;
+// TASK N:
+
+// Shunday function yozing, u string qabul qilsin va string palindrom yani togri oqilganda ham, orqasidan oqilganda ham bir hil oqiladigan soz ekanligini aniqlab boolean qiymat qaytarsin.
+
+// MASALAN: palindromCheck("dad") return true;  palindromCheck("son") return false;
+function palindromCheck(str) {
+  return str === str.split("").reverse().join("");
 }
-const answer = getSquareNumbers([1, 2, 3]);
-console.log(answer);
+
+const result = palindromCheck("dad");
+const result2 = palindromCheck("son");
+
+console.log("Result: ", result);
+console.log("Result2: ", result2);
+
+// function getSquareNumbers(numbs) {
+// //   let result = [];
+// //   for (let i = 0; i < numbs.length; i++) {
+// //     let number = numbs[i];
+// //     result.push({ number: number, square: number * number });
+// //   }
+// //   return result;
+// // }
+// // const answer = getSquareNumbers([1, 2, 3]);
+// // console.log(answer);
